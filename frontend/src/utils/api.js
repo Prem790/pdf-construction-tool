d9239391d3757ebc4e545266eae9689484e7b6da
Prem Jadwani <pjadwani_be21@thapar.edu>
2025-09-08 01:44:16 +0530

    Fixed some issue in frontend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'https://pdf-construction-tool.onrender.com';
+const API_BASE = 'https://pdf-construction-tool.onrender.com/api';
 // const API_BASE = 'http://localhost:8000/api';
 
 const api = axios.create({
